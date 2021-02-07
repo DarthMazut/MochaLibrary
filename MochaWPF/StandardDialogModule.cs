77e1c1bb1ec96522b8576f5e1f68e3482dd1eeab
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2021-02-07 17:03:55 +0100

    Added documentation for StandardDialogModule

--- MochaWPF/StandardDialogModule.cs
+++ MochaWPF/StandardDialogModule.cs
@@ -8,57 +8,109 @@ using System.Windows;
 
 namespace MochaWPF
 {
+    /// <summary>
+    /// Provides an implementation of windows <see cref="MessageBox"/> for WPF app.
+    /// </summary>
     public class StandardDialogModule : IDialogModule
     {
         private IDialog _dataContext;
         private bool _isOpen = false;
         private Func<IDialog, Window> _getParentWindow;
 
+        /// <summary>
+        /// There is no view object which represents WPF <see cref="MessageBox"/> dialog
+        /// therefore this always returns <see langword="null"/>.
+        /// </summary>
         public object View => null;
 
+        /// <summary>
+        /// Returns an <see cref="IDialog"/> object which serve as a backend
+        /// for represented <see cref="MessageBox"/>. 
+        /// </summary>
         public IDialog DataContext => _dataContext;
 
+        /// <summary>
+        /// Indicates whether the represented <see cref="MessageBox"/> is currently open.
+        /// </summary>
         public bool IsOpen => _isOpen;
 
+        /// <summary>
+        /// Fires when represented <see cref="MessageBox"/> closes.
+        /// </summary>
         public event EventHandler Closed;
 
+        /// <summary>
+        /// Fires when this instance is done.
+        /// </summary>
         public event EventHandler Disposed;
 
+        /// <summary>
+        /// Default construtcor. Uses build-in simple implementaion of <see cref="IDialog"/>.
+        /// </summary>
         public StandardDialogModule() : this(null, null) { }
 
+        /// <summary>
+        /// Returns new instance of <see cref="StandardDialogModule"/> class.
+        /// </summary>
+        /// <param name="dialogData">Backend for represented <see cref="MessageBox"/> dialog.</param>
         public StandardDialogModule(IDialog dialogData) : this(dialogData, null) { }
 
+        /// <summary>
+        /// Returns new instance of <see cref="StandardDialogModule"/> class.
+        /// </summary>
+        /// <param name="dialogData">Backend for represented <see cref="MessageBox"/> dialog.</param>
+        /// <param name="getParentWindow">A delegate which returns parent window for this dialog.</param>
         public StandardDialogModule(IDialog dialogData, Func<IDialog, Window> getParentWindow)
         {
             _dataContext = dialogData ?? new SimpleDialog();
             _getParentWindow = getParentWindow;
         }
 
+        /// <summary>
+        /// This method is not supported by <see cref="StandardDialogModule"/> implementation.
+        /// </summary>
         public void Close()
         {
             throw new NotSupportedException("Closing StandardDialogModule is currently not supported in MochaLib :(");
         }
 
+        /// <summary>
+        /// Marks this dialog as done.
+        /// </summary>
         public void Dispose()
         {
             Disposed?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        /// Sets backend data for this dialog.
+        /// </summary>
+        /// <param name="dialogData">Backend data to be set.</param>
         public void SetDataContext(IDialog dialogData)
         {
             _dataContext = dialogData;
         }
 
+        /// <summary>
+        /// Showing <see cref="MessageBox"/> in non-modal way is not supported by this implementation.
+        /// </summary>
         public void Show()
         {
             throw new NotSupportedException("StandardDialogModule can only be displayed as modal.");
         }
 
+        /// <summary>
+        /// Showing <see cref="MessageBox"/> asynchronously in non-modal way is not supported by this implementation.
+        /// </summary>
         public Task ShowAsync()
         {
             throw new NotSupportedException("StandardDialogModule can only be displayed as modal.");
         }
 
+        /// <summary>
+        /// Displays represented <see cref="MessageBox"/> dialog in modal manner.
+        /// Returns result of dialog interaction.
+        /// </summary>
         public bool? ShowModal()
         {
             _isOpen = true;
@@ -80,6 +132,10 @@ namespace MochaWPF
             return result;
         }
 
+        /// <summary>
+        /// Displays asynchronously represented <see cref="MessageBox"/> dialog in modal manner.
+        /// Returns result of dialog interaction.
+        /// </summary>
         public Task<bool?> ShowModalAsync()
         {
             return Task.Run(() => 
