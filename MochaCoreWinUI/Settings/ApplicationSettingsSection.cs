a50556570b4f7c520fc760f1c85303ced79fe7ac
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2021-12-25 12:52:42 +0100

    Add ApplicationSettingsSection for WinUI - WIP

--- MochaCoreWinUI/Settings/ApplicationSettingsSection.cs
+++ MochaCoreWinUI/Settings/ApplicationSettingsSection.cs
@@ -1,27 +1,58 @@
 ﻿using MochaCore.Settings;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace MochaCoreWinUI.Settings
 {
     public class ApplicationSettingsSection<T> : ISettingsSection<T> where T : class, new()
     {
-        public T Load()
+        private readonly StorageFolder _settingsFolder = ApplicationData.Current.LocalFolder;
+        private readonly string _settingName;
+        private readonly Func<T, Task<string>> _serializationDelegate;
+        private readonly Func<string, Task<T>> _deserializationDelegate;
+
+        public ApplicationSettingsSection(Func<T, Task<string>> serializationDelegate, Func<string, Task<T>> deserializationDelegate, string settingName)
         {
-            throw new NotImplementedException();
+            _serializationDelegate = serializationDelegate;
+            _deserializationDelegate = deserializationDelegate;
+            _settingName = settingName;
         }
 
-        public Task<T> LoadAsync()
+        /// <summary>
+        /// SYNC API WILL BE REMOVED FROM MOCHALIB.
+        /// </summary>
+        public T Load()
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
+        }
+
+        // Add thread-safety
+        // Throw when delegate fails (InnerException)
+
+        public async Task<T> LoadAsync()
+        {
+            IStorageItem storageItem = await _settingsFolder.TryGetItemAsync(_settingName);
+            if (storageItem is StorageFile foundStorageFile)
+            {
+                string fileContent = await FileIO.ReadTextAsync(foundStorageFile);
+                return await _deserializationDelegate.Invoke(fileContent);
+            }
+
+            StorageFile newStorageFile = await _settingsFolder.CreateFileAsync(_settingName);
+            T settingObject = new();
+            string serializedObject = await _serializationDelegate.Invoke(settingObject);
+            await FileIO.WriteTextAsync(newStorageFile, serializedObject);
+            return settingObject;
         }
 
         public void RestoreDefaults()
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public Task RestoreDefaultsAsync()
@@ -31,7 +62,7 @@ namespace MochaCoreWinUI.Settings
 
         public void Save(T settings)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public Task SaveAsync(T settings)
@@ -41,7 +72,7 @@ namespace MochaCoreWinUI.Settings
 
         public void Update(Action<T> updateAction)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public Task UpdateAsync(Action<T> updateAction)
