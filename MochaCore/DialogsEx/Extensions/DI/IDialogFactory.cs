be099e407f20094c61f3cfb5f6284f391d858b7b
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2022-03-22 13:36:50 +0100

    Add DI to DialogsEx

---
+++ MochaCore/DialogsEx/Extensions/DI/IDialogFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaCore.DialogsEx.Extensions.DI
+{
+    /// <summary>
+    /// Provides instances of dialogs registered by <see cref="DialogManager"/> class.
+    /// </summary>
+    public interface IDialogFactory
+    {
+        /// <summary>
+        /// Returns new instance of <see cref="IDialogModule{T}"/> corresponding to given indetifier.
+        /// </summary>
+        /// <typeparam name="T">Type of properties object used by created dialog.</typeparam>
+        /// <param name="id">Specifies the dialog identifier.</param>
+        IDialogModule<T> Create<T>(string id);
+
+        /// <summary>
+        /// Returns new instacne of <see cref="IDataContextDialogModule{T}"/> corresponding to given identifier.
+        /// </summary>
+        /// <typeparam name="T">Type of properties object used by created dialog.</typeparam>
+        /// <param name="id">Dialog identifier.</param>
+        IDataContextDialogModule<T> CreateDataContextModule<T>(string id);
+
+        /// <summary>
+        /// Returns new instacne of <see cref="ICustomDialogModule{T}"/> corresponding to given identifier.
+        /// </summary>
+        /// <typeparam name="T">Type of properties object used by created dialog.</typeparam>
+        /// <param name="id">Dialog identifier.</param>
+        ICustomDialogModule<T> CreateCustomModule<T>(string id);
+
+        /// <summary>
+        /// Returns a collection of currently open <see cref="IDialogModule"/> with given ID.
+        /// </summary>
+        /// <param name="id">Specifies the dialog identifier.</param>
+        List<IDialogModule> GetOpenedDialogs(string id);
+
+        /// <summary>
+        /// Returns a collection of all currently open <see cref="IDialogModule"/>.
+        /// </summary>
+        List<IDialogModule> GetOpenedDialogs();
+
+    }
+}
