d367ed09b97dd65f0c930c266c7ff4e99c8f9092
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2023-05-04 19:33:49 +0200

    Add NavigationStackItem

---
+++ MochaCore/NavigationEx/INavigationStackItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaCore.NavigationEx
+{
+    public interface INavigationStackItem
+    {
+        public INavigationModule Module { get; }
+
+        public bool IsModalOrigin { get; }
+    }
+}
---
+++ MochaCore/NavigationEx/NavigationStackItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaCore.NavigationEx
+{
+    public class NavigationStackItem : INavigationStackItem
+    {
+        public NavigationStackItem(INavigationLifecycleModule module) : this(module, null) { }
+
+        public NavigationStackItem(INavigationLifecycleModule module, TaskCompletionSource<object?>? modalCompletionSource)
+        {
+            Module = module ?? throw new ArgumentNullException(nameof(module));
+            ModalCompletionSource = modalCompletionSource;
+        }
+
+        INavigationModule INavigationStackItem.Module => Module;
+
+        public INavigationLifecycleModule Module { get; }
+
+        public TaskCompletionSource<object?>? ModalCompletionSource { get; }
+
+        public bool IsModalOrigin => ModalCompletionSource is not null;     
+    }
+}
