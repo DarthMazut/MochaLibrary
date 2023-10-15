8c9d9639f3069cc9b0afa4bec2936fc55a82f205
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2023-10-15 19:29:45 +0200

    Add DI for Navigation

---
+++ MochaCore/Navigation/Extensions/DI/NavigationServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaCore.Navigation.Extensions.DI
+{
+    /// <summary>
+    /// Provides implementation of <see cref="INavigationServiceProvider"/>.
+    /// </summary>
+    public class NavigationServiceProvider : INavigationServiceProvider
+    {
+        /// <inheritdoc/>
+        public INavigationService FetchNavigationService(string id) => NavigationManager.FetchNavigationService(id);
+    }
+}
