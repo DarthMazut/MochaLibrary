da1e6d318a7a1192d6c5b2b5c67085cd1816267d
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-05-14 16:09:13 +0200

    Add RandomNumber

---
+++ MochaWinUI/Utils/Xaml/UniversalConverter/Expressions/RandomNumber.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Markup;
+using MochaCore.Utils.Xaml.UniversalConverter;
+using MochaCore.Utils.Xaml.UniversalConverter.CoreExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaWinUI.Utils.Xaml.UniversalConverter.Expressions
+{
+    public class RandomNumber : MarkupExtension, IConvertingExpression
+    {
+        public int? LowerBound { get; set; }
+
+        public int? UpperBound { get; set; }
+
+        public bool IsConditionExpression => false;
+
+        public object? CalculateExpression(object? value)
+            => new CoreRandomNumber()
+            {
+                LowerBound = LowerBound,
+                UpperBound = UpperBound
+            }.CalculateExpression(value);
+
+        protected override object ProvideValue(IXamlServiceProvider serviceProvider)
+        {
+            return new RandomNumber()
+            {
+                LowerBound = LowerBound,
+                UpperBound = UpperBound
+            };
+        }
+    }
+}
