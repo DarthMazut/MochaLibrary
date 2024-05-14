0e32a258096f7a5ca1b66ad84480acc1afbe9d6a
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-05-14 21:30:25 +0200

    Add Negate

---
+++ MochaWinUI/Utils/Xaml/UniversalConverter/Expressions/Negate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml.Markup;
+using MochaCore.Utils.Xaml.UniversalConverter;
+using MochaCore.Utils.Xaml.UniversalConverter.CoreExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaWinUI.Utils.Xaml.UniversalConverter.Expressions
+{
+    public class Negate : MarkupExtension, IConvertingExpression
+    {
+        public bool IsConditionExpression => true;
+
+        public object? CalculateExpression(object? value) => new CoreNegate().CalculateExpression(value);
+    }
+}
