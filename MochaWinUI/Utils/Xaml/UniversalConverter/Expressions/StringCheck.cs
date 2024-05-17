8eef50cdfbbd8e13ba4aeb1167824d075dad38a9
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-05-17 22:38:42 +0200

    Add StringCheck

---
+++ MochaWinUI/Utils/Xaml/UniversalConverter/Expressions/StringCheck.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Markup;
+using MochaCore.Utils.Xaml.UniversalConverter;
+using MochaCore.Utils.Xaml.UniversalConverter.CoreExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaWinUI.Utils.Xaml.UniversalConverter
+{
+    public class StringCheck : MarkupExtension, IConvertingExpression
+    {
+        public StringCheckOperation? CheckOperation { get; set; }
+
+        public string? StartsWith { get; set; }
+
+        public string? EndsWith { get; set; }
+
+        public string? Contains { get; set; }
+
+        public bool IsConditionExpression => true;
+
+        public object? CalculateExpression(object? value) => new CoreStringCheck()
+        {
+            CheckOperation = CheckOperation,
+            StartsWith = StartsWith,
+            EndsWith = EndsWith,
+            Contains = Contains
+        }.CalculateExpression(value);
+
+        protected override object ProvideValue(IXamlServiceProvider serviceProvider) => new StringCheck()
+        {
+            CheckOperation = CheckOperation,
+            StartsWith = StartsWith,
+            EndsWith = EndsWith,
+            Contains = Contains
+        };
+    }
+}
