fe67c83a5b7580a8750d57770618f0bc7d33008c
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-07-12 21:08:26 +0200

    WIP

---
+++ ViewModelsX/Pages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModelsX
+{
+    public static class Pages
+    {
+        public static
+    }
+
+    public class AppPage
+    {
+        public string Name { get; }
+
+        public int MyProperty { get; set; }
+    }
+}
