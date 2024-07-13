68ce50d9781497cedbca9aba1e176952de338dc0
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-07-13 17:10:45 +0200

    WIP

--- ViewModelsX/Pages.cs
+++ ViewModelsX/Pages.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,33 @@ namespace ViewModelsX
 {
     public static class Pages
     {
-        public static
+        private static Dictionary<string, AppPage> _pages = new()
+        {
+            { 
+                nameof(HomePage), new AppPage(nameof(HomePage)) 
+                {
+                    Name = "Home Page",
+                    Glyph = "\uE80F",
+                    IsMenuPage = true
+                }
+            },
+        };
+
+        public static AppPage HomePage => _pages[nameof(HomePage)];
+
+        public static IReadOnlyCollection<AppPage> AsCollection() => [.. _pages.Values];
+
+        public static IReadOnlyCollection<AppPage> GetMenuPages() => _pages.Values.Where(p => p.IsMenuPage).ToImmutableList();
     }
 
-    public class AppPage
+    public record AppPage(string Id)
     {
-        public string Name { get; }
+        public string Name { get; init; } = string.Empty;
 
-        public int MyProperty { get; set; }
+        public string Glyph { get; init; } = string.Empty;
+
+        public bool IsMenuPage { get; init; } = false;
+
+        public bool IsFullScreen { get; init; } = false;
     }
 }
---
+++ WinUiApplicationX/Utils/Xaml/MenuPagesExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Markup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUiApplicationX.Utils.Xaml
+{
+    public class MenuPagesExtension : MarkupExtension
+    {
+        protected override object ProvideValue() => ViewModelsX.Pages.GetMenuPages();
+    }
+}
