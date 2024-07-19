f3f5d09e1c18f9607efa4d60394625b9564ff343
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-07-19 21:33:24 +0200

    Add crypto

--- ModelX/Settings.cs
+++ ModelX/Settings.cs
@@ -11,6 +11,8 @@ namespace ModelX
 
         public string? Text { get; set; }
 
+        public string? CryptoText { get; set; }
+
         public async Task FillValuesAsync(string serializedData)
         {
             Settings? settings = await Task.Run(() => JsonSerializer.Deserialize<Settings>(serializedData));
@@ -19,6 +21,7 @@ namespace ModelX
                 Switch1 = settings.Switch1;
                 OptionType = settings.OptionType;
                 Text = settings.Text;
+                CryptoText = settings.CryptoText;
             }
         }
 
--- ViewModelsX/Pages/SettingsPageViewModel.cs
+++ ViewModelsX/Pages/SettingsPageViewModel.cs
@@ -6,6 +6,7 @@ using ModelX;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,6 +32,12 @@ namespace ViewModelsX.Pages
         [ObservableProperty]
         private string? _text;
 
+        [ObservableProperty]
+        private string? _password;
+
+        [ObservableProperty]
+        private string? _cryptoText;
+
         public async Task OnNavigatedToAsync(OnNavigatedToEventArgs e)
         {
             try
@@ -39,6 +46,7 @@ namespace ViewModelsX.Pages
                 IsSwitched = settings.Switch1;
                 DropDownSelectedItem = settings.OptionType;
                 Text = settings.Text;
+                CryptoText = settings.CryptoText;
             }
             catch (IOException)
             {
@@ -78,6 +86,58 @@ namespace ViewModelsX.Pages
                 // TODO: handle
                 throw;
             }
+        }
+
+        [RelayCommand]
+        private async Task Decrypt()
+        {
+            if (CryptoText is null || Password is null)
+            {
+                return;
+            }
+
+            CryptoText = await DecryptTextAsync(CryptoText, Password);
+        }
+
+        [RelayCommand]
+        private async Task Encrypt()
+        {
+            if (CryptoText is null || Password is null)
+            {
+                return;
+            }
+
+            string encryptedText = await EncryptTextAsync(CryptoText, Password);
+            await _settingsProvider.UpdateAsync(s => s.CryptoText = encryptedText);
+        }
+
+        private static async Task<string> EncryptTextAsync(string plainText, string password)
+        {
+            using Aes aes = Aes.Create();
+            using Rfc2898DeriveBytes keyGenerator = new(password, Encoding.UTF8.GetBytes("SaltIsGoodForYou"), 1000, HashAlgorithmName.SHA1);
+            aes.Key = keyGenerator.GetBytes(32);
+            aes.IV = keyGenerator.GetBytes(16);
+
+            using MemoryStream memoryStream = new();
+            memoryStream.Write(aes.IV, 0, aes.IV.Length);
+            using CryptoStream cryptoStream = new(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            using StreamWriter streamWriter = new(cryptoStream);
+            await streamWriter.WriteAsync(plainText);
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+
+        private static Task<string> DecryptTextAsync(string cipherText, string password)
+        {
+            using Aes aes = Aes.Create();
+            using Rfc2898DeriveBytes keyGenerator = new(password, Encoding.UTF8.GetBytes("SaltIsGoodForYou"), 1000, HashAlgorithmName.SHA1);
+
+            aes.Key = keyGenerator.GetBytes(32);
+            aes.IV = keyGenerator.GetBytes(16);
+
+            using MemoryStream memoryStream = new(Convert.FromBase64String(cipherText));
+            using CryptoStream cryptoStream = new(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using StreamReader streamReader = new(cryptoStream);
+            return streamReader.ReadToEndAsync();
         }
 
 
