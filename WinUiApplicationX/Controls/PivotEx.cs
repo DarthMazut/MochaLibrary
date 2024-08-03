f03e422d2467ad8b44c75be9fe8b9b38576c44c3
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-08-03 20:47:49 +0200

    WIP

---
+++ WinUiApplicationX/Controls/PivotEx.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUiApplicationX.Controls
+{
+    public class PivotEx : Pivot
+    {
+        public PivotEx()
+        {
+            this.DefaultStyleKey = typeof(PivotEx);
+        }
+    }
+
+    public class PivotHeaderItemEx : PivotHeaderItem
+    {
+        public PivotHeaderItemEx()
+        {
+            this.DefaultStyleKey = typeof(PivotHeaderItemEx);
+        }
+    }
+}
