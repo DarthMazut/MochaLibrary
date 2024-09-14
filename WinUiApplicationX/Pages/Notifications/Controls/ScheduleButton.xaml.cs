ce42f2c74e83da1f32b9d71519ef623653ab9ad0
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-09-14 21:29:06 +0200

    Extract ScheduleButton

---
+++ WinUiApplicationX/Pages/Notifications/Controls/ScheduleButton.xaml.cs
@@ -0,0 +1,72 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUiApplicationX.Pages.Notifications.Controls
+{
+    public sealed partial class ScheduleButton : UserControl
+    {
+        public static readonly DependencyProperty IsScheduableProperty =
+            DependencyProperty.Register(nameof(IsScheduable), typeof(bool), typeof(ScheduleButton), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty ProgressValueProperty =
+            DependencyProperty.Register(nameof(ProgressValue), typeof(double), typeof(ScheduleButton), new PropertyMetadata(0));
+        
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(ScheduleButton), new PropertyMetadata(null));
+
+        public bool IsScheduable
+        {
+            get => (bool)GetValue(IsScheduableProperty);
+            set => SetValue(IsScheduableProperty, value);
+        }
+
+        public double ProgressValue
+        {
+            get => (double)GetValue(ProgressValueProperty);
+            set => SetValue(ProgressValueProperty, value);
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public ScheduleButton()
+        {
+            this.InitializeComponent();
+            RegisterPropertyChangedCallback(IsScheduableProperty, OnScheduableChanged);
+        }
+
+        private void OnScheduableChanged(DependencyObject sender, DependencyProperty dp)
+        {
+            if (sender is ScheduleButton thisControl)
+            {
+                if (thisControl.IsScheduable)
+                {
+
+                }
+                else
+                {
+
+                }
+            } 
+        }
+    }
+}
