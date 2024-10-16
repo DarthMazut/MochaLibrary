83d556235e5f45425315f901dcb17f07ccc07f81
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-10-16 20:00:19 +0200

    WIP

--- ViewModelsX/Pages/Sandbox/SandboxPageViewModel.cs
+++ ViewModelsX/Pages/Sandbox/SandboxPageViewModel.cs
@@ -1,16 +1,31 @@
-﻿using MochaCore.Navigation;
+﻿using CommunityToolkit.Mvvm.Input;
+using MochaCore.Dialogs;
+using MochaCore.Dialogs.Extensions;
+using MochaCore.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModelsX.Dialogs;
 
 namespace ViewModelsX.Pages.Sandbox
 {
-    public class SandboxPageViewModel : INavigationParticipant
+    public partial class SandboxPageViewModel : INavigationParticipant
     {
         public INavigator Navigator { get; } = MochaCore.Navigation.Navigator.Create();
 
-        public string Text => "Hello sandbox ;)";
+        [RelayCommand]
+        private Task Test()
+        {
+            using ICustomDialogModule<StandardMessageDialogProperties> dialogModule = AppDialogs.StandardMessageDialog.Module;
+            dialogModule.Properties = new StandardMessageDialogProperties()
+            {
+                Title = "Test method",
+                Message = "Clicked"
+            };
+
+            return dialogModule.ShowModalAsync(Navigator.Module.View);
+        }
     }
 }
